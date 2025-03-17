aebcec925cf04f7d804b89247505449661c5d608
Aleksander Molostov <molostov.alex.nik@gmail.com>
2025-03-17 13:44:03 +0300

    доправлены неймспейсы

--- src/DataLibrary/Migrations/20250313140320_InitialCreate.cs
+++ src/DataLibrary/Migrations/20250313140320_InitialCreate.cs
@@ -4,95 +4,94 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace ConnectionLogger.Data.Migrations
+namespace ConnectionLogger.Data.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "IpAddresses",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    Address = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: false),
-                    Protocol = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_IpAddresses", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "IpAddresses",
+            columns: table => new
+            {
+                Id = table.Column<long>(type: "bigint", nullable: false)
+                    .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                Address = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: false),
+                Protocol = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_IpAddresses", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    LastName = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: false),
-                    FirstName = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Id);
-                });
+        migrationBuilder.CreateTable(
+            name: "Users",
+            columns: table => new
+            {
+                Id = table.Column<long>(type: "bigint", nullable: false)
+                    .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                LastName = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: false),
+                FirstName = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Users", x => x.Id);
+            });
 
-            migrationBuilder.CreateTable(
-                name: "Connections",
-                columns: table => new
-                {
-                    UserId = table.Column<long>(type: "bigint", nullable: false),
-                    IpAddressId = table.Column<long>(type: "bigint", nullable: false),
-                    ConnectedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Connections", x => new { x.UserId, x.IpAddressId, x.ConnectedAt });
-                    table.ForeignKey(
-                        name: "FK_Connections_IpAddresses_IpAddressId",
-                        column: x => x.IpAddressId,
-                        principalTable: "IpAddresses",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Connections_Users_UserId",
-                        column: x => x.UserId,
-                        principalTable: "Users",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "Connections",
+            columns: table => new
+            {
+                UserId = table.Column<long>(type: "bigint", nullable: false),
+                IpAddressId = table.Column<long>(type: "bigint", nullable: false),
+                ConnectedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Connections", x => new { x.UserId, x.IpAddressId, x.ConnectedAt });
+                table.ForeignKey(
+                    name: "FK_Connections_IpAddresses_IpAddressId",
+                    column: x => x.IpAddressId,
+                    principalTable: "IpAddresses",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Connections_Users_UserId",
+                    column: x => x.UserId,
+                    principalTable: "Users",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Connections_IpAddressId",
-                table: "Connections",
-                column: "IpAddressId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Connections_IpAddressId",
+            table: "Connections",
+            column: "IpAddressId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Connections_UserId",
-                table: "Connections",
-                column: "UserId");
+        migrationBuilder.CreateIndex(
+            name: "IX_Connections_UserId",
+            table: "Connections",
+            column: "UserId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_IpAddresses_Address",
-                table: "IpAddresses",
-                column: "Address",
-                unique: true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_IpAddresses_Address",
+            table: "IpAddresses",
+            column: "Address",
+            unique: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Connections");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Connections");
 
-            migrationBuilder.DropTable(
-                name: "IpAddresses");
+        migrationBuilder.DropTable(
+            name: "IpAddresses");
 
-            migrationBuilder.DropTable(
-                name: "Users");
-        }
+        migrationBuilder.DropTable(
+            name: "Users");
     }
 }
